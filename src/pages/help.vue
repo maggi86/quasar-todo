e313bd0de278d0d2d38749ab1947a55bc4e10370
maggi86 <meaganwilliams407@gmail.com>
2022-09-29 15:05:14 +0200

    irst

---
+++ src/pages/help.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-page class="q-pa-lg">
+      <h5 class="q-mt-none">
+        help
+      </h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis modi ad velit consequuntur! Perspiciatis suscipit sunt, maxime in distinctio quam eaque quis, minima cum odio iste, modi reprehenderit consequuntur eos.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam consequatur, labore hic quis rerum aliquam sequi provident? Mollitia voluptas pariatur sit odio repellat provident et! Enim consequuntur incidunt recusandae dolorum.</p>
+    </q-page>
+  </template>
+  
+  <script>
+  export default{
+    
+  }
+  </script>
---
+++ src/pages/todo.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <div class="q-pa-md">
+    <div class="q-gutter-sm">
+    <q-list>
+      <!--
+        Rendering a <label> tag (notice tag="label")
+        so QCheckboxes will respond to clicks on QItems to
+        change Toggle state.
+      -->
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox  keep-color v-model="secondary" color="secondary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Teal</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="orange" color="orange" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Orange</q-item-label>
+          <q-item-label caption>With description</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar top>
+          <q-checkbox v-model="color" val="cyan" color="cyan" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Cyan</q-item-label>
+          
+        </q-item-section>
+      </q-item>
+    </q-list>
+    </div>
+
+    <div class="q-px-sm q-mt-sm">
+      Your selection is: <strong>{{ color }}</strong>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default{
+
+}
+</script>
